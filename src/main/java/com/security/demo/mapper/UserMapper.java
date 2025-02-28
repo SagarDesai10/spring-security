7b7252a5c973511776f691eed7d0643ebaf11c85
SagarDesai10 <desaisagar8090@gmail.com>
2025-02-28 23:36:44 +0530

    minore modifiation in mapper

--- src/main/java/com/security/demo/mapper/UserMapper.java
+++ src/main/java/com/security/demo/mapper/UserMapper.java
@@ -1,22 +1,27 @@
 package com.security.demo.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
 
 import com.security.demo.dto.UserResponseDTO;
 import com.security.demo.entity.User;
 
 
 @Named("UserMapper")
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 	@Mappings({ @Mapping(target = "id", source = "entity.id"),
 			@Mapping(target = "name", source = "entity.name"),
 			@Mapping(target = "role", source = "entity.role"),
 			@Mapping(target = "email", source = "entity.email")})
 	UserResponseDTO mapToDTO(User entity);
+	
+	List<UserResponseDTO> mapToDtoList(List<User> entity);
 	
 	
 
